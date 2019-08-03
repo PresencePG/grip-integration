3ecfab05ee48455b49e68243f72e8b909d1a214d
John Slinkman <john@packetizedenergy.com>
2019-08-03 15:56:20 -0400

    Simple unit test for sending a message

--- julia/server.jl
+++ julia/server.jl
@@ -5,17 +5,21 @@ ctx = Context()
 
 connection_uri = ENV["SERVER_LISTEN_URI"]
 
-reciever = Socket(ctx, PAIR)
-ZMQ.bind(reciever, connection_uri)
+receiver = Socket(ctx, PAIR)
+ZMQ.bind(receiver, connection_uri)
 
 println("Julia ready . . .")
 
 while true
-    raw_msq = recv(reciever, String)
+    raw_msq = recv(receiver, String)
     msg = JSON.parse(raw_msq)
     # Do work here
     sent_at = msg["at"]
     message = msg["message"]
-    send(reciever, "Recieved message sent at '$(sent_at)'")
+    if message == "ping"
+        send(receiver, "pong")
+        continue
+    end
+    send(receiver, "Recieved message sent at '$(sent_at)'")
     println("$(sent_at): $(message)")
 end
