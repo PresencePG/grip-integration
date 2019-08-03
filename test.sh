417d69d75ba2809b68641f4c20f415ffd6ae724f
phines <paul.hines@uvm.edu>
2019-08-03 18:44:44 -0400

    Deleted test.sh

--- test.sh
+++
@@ -1,9 +0,0 @@
-echo "------------------------------------------------"
-echo "Running a test of python/julia communications"
-echo "------------------------------------------------"
-python python/ping_pong.py
-
-echo "------------------------------------------------"
-echo "Running a simple julia optimization test"
-echo "------------------------------------------------"
-#julia julia/test_jump.jl
