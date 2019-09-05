48d1384a32cbe2ce2f6810e4f05d8f0a2c11457d
sehowerter <sehowerter@gmail.com>
2019-09-05 09:39:42 -0400

    updatereq

--- julia/requirements.jl
+++ julia/requirements.jl
@@ -3,21 +3,21 @@ using Pkg;
 Pkg.add("JSON")
 Pkg.add("ZMQ")
 Pkg.add("JuMP")
-Pkg.add("Clp")
 Pkg.add("Cbc")
 Pkg.add("CSV")
 Pkg.add("DataFrames")
 Pkg.add("Test")
 Pkg.add("SparseArrays")
+Pkg.add("LightGraphs")
 
 using JSON
 using ZMQ
 using JuMP
 using Cbc
-using Clp
 using CSV
 using DataFrames
 using Test
 using SparseArrays
+using LightGraphs
 
 println("Julia package installation complete.")
